ad1e15eea62ea7b81b8bc2660ab315f0280fac88
unon <unon>
2017-06-11 11:29:34 -0500

    wip

--- src/components/quranSynonym.vue
+++ src/components/quranSynonym.vue
@@ -4,11 +4,19 @@
 	<span class=text-muted>
 		( {{id}} )
 	</span>
-	<div v-html='data'>
+	<HR/><BR/>
+
+	<div v-if="loading">
+		Loading...
 	</div>
-	<div class=text-muted>
+	<div v-else-if="error">
+		ERROR:<BR/> {{ error }}
+	</div>
+	<div v-else v-html='data'>
+	</div>
+<!-- 	<div class=text-muted>
 		{{data}}
-	</div>
+	</div> -->
   </div>
 </template>
 
@@ -19,6 +27,8 @@
 
 		data: function(){
 			return {
+				error: null,
+				loading: null,
 				data: null,
 			};
 		},
@@ -45,9 +55,18 @@
 				comp.number = +comp.id.replace(/[^\d]+/, ''); //returns 12 for b12
 				comp.path = '../data/content/' + comp.section;
 				comp.lookup = 'content/' + comp.section +'/' + comp.id + '.html';
+				//debugger;
 	    		require([ '../data/content/' + comp.section ], function( data ){
-	    			console.log( data );
+//	    			console.log( data );
 	    			comp.data = data[ comp.lookup ];
+	    			if(!comp.data){
+	    				comp.error = "No entry found for " + comp.id + " in section " + comp.section;
+	    			}
+	    			comp.loading = false;
+	    		},
+	    		function(error){
+	    			comp.error = (error && error.message) ? error.message : error;
+	    			comp.loading = false;
 	    		})
 	    	},
 	    },
