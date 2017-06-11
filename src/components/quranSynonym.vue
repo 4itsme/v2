bdabcf8e0ac69a1f2cda6ba8e061654fd959bb13
unon <unon>
2017-06-11 10:33:44 -0500

    wip

--- src/components/quranSynonym.vue
+++ src/components/quranSynonym.vue
@@ -4,6 +4,11 @@
 	<span class=text-muted>
 		( {{id}} )
 	</span>
+	<div v-html='data'>
+	</div>
+	<div class=text-muted>
+		{{data}}
+	</div>
   </div>
 </template>
 
@@ -11,5 +16,36 @@
 <script>
 	export default {
 		props: ['id'],
+
+		data: function(){
+			return {
+				data: null,
+			};
+		},
+		created: function() {
+		    // fetch the data when the view is created and the data is
+		    // already being observed
+		    this.fetchData()
+	    },
+	    watch: {
+	    	$route: function(to, from) {
+		      // react to route changes...
+		      this.fetchData();
+		    },
+	    },
+	    methods: {
+	    	fetchData: function(){
+	    		//this.data.id = + new Date();
+	    		this.error = /*this.data =*/ null;
+	    		this.loading = true;
+	    		this.data = null;
+	    		var comp = this;
+	    		require(['../data/content/b'], function( b ){
+	    			console.log(b);
+	    			comp.data = b[ 'content/b/b1.html' ];
+	    		})
+	    	},
+	    },
+
 	}
 </script>
